b105fcc2cd178fb9a256550401a205fcfcef9719
Robin Bank <robinbank@gmail.com>
2015-08-15 00:39:34 -0400

    functioning now
    basic functionality works now

--- jquery.tscAccordion.js
+++ jquery.tscAccordion.js
@@ -1,25 +1,23 @@
-
 /** 
- * TSC Accordian
+ * TSC Accordion
  * Robin Bank, Aug 2015
  *
  *
-*/
+ */
 
-; (function ($, window, document, undefined) {
 
-    var pluginName = 'tscAccordian',
+(function($, window, document, undefined) {
+    var pluginName = 'tscAccordion',
         defaults = {
             openClassName: "open",
         };
-    var accGroups = [];   //saved accordians
-    var accCollection = {};
-
+    var accGroups = []; //saved accordion selection
+    var accCollection = [];  //collection of groups
 
     function Plugin(el, options) {
         this.el = el;
         this.options = $.extend({}, defaults, options);
-        
+
         this._defaults = defaults;
         this._name = pluginName;
         this.init();
@@ -27,91 +25,116 @@
 
     Plugin.prototype = {
 
-        init: function () {
+        init: function() {
             this.activeRow = null;
-
-
-            /**
-             * Hook up initial events
-             */
             this._findAllGroups();
-            //this._setupAcc();
             this._bindEvents();
-            
-        
+            this._setDefaultStates();
+            //this._addButtonMarkup();
         },
 
-        _findAllGroups: function () {
-            accGroups = $('[accordian]').filter(function () {
+        // Private methods start with underscore
+        
+        _findAllGroups: function() {
+            accGroups = $('[accordian]').filter(function() {
                 return $(this).attr('accordian').toLowerCase().indexOf('true') > -1;
             });
-            
-            //build the Accordian Collection
-            
+
+            //build the Accordion Collection
             for (var i = 0; i < accGroups.length; i++) {
                 accCollection[i] = {}; //initial sub object
-
                 accCollection[i].element = accGroups[i];
-                accCollection[i].auto = $(accGroups[i]).attr('autoclose').toLowerCase();
+                accCollection[i].autoclose = $(accGroups[i]).attr('autoclose').toLowerCase();
                 accCollection[i].collapsed = $(accGroups[i]).attr('collapsed').toLowerCase();
-                
                 if ($(accGroups[i]).prop('nodeName') == 'H2') {
-                        accCollection[i].parent = $(accGroups[i]).parent().next().get(0);
+                    accCollection[i].elementTarget = $(accGroups[i]).siblings().get(0);
+                    accCollection[i].elementParent = $(accGroups[i]).parent();
                 }
                 if ($(accGroups[i]).prop('nodeName') == 'SPAN') {
-                        accCollection[i].parent = $(accGroups[i]).next().get(0);
+                    accCollection[i].elementTarget = $(accGroups[i]).next().get(0);
                 }
-                
             }
         },
 
-        _bindEvents: function () {
-            
-            for (var key in accCollection) {
-                debugger
-                $(accCollection[key].element).on('click',  function () {
-                    debugger;
+        _toggleState: function(key) {
+            //if open then close
+            $(accCollection[key].elementParent).toggleClass('collapse');
+
+        },
+
+        _bindEvents: function() {
+            for (var i = 0; i < accCollection.length; i++) {
+                $(accCollection[i].element).on('click', {
+                    elementTarget: accCollection[i].elementTarget,
+                    autoclose: accCollection[i].autoclose,
+                    key: i,
+                    that: this
+                }, function(event) {
+                    $(event.data.that._toggleState(event.data.key));
+                    //if current menu is auto run autoclose
                     
-                    $(accCollection[key].parent).toggle();
+                    if(event.data.autoclose=="true"){
+                        $(event.data.that._autoClose(event.data.key))    
+                    }
+                    
                 });
             }
-            
+        },
+
+        _autoClose: function(groupKeepOpen) {
+            for (var i = 0; i < accCollection.length; i++) {
+                if (groupKeepOpen != i) {
+                    if (accCollection[i].autoclose == 'true') {
+                        //close the item
+                        //debugger;
+                        this._setState(i, 'close');
+                    }
+                }
+            }
+        },
+
+        _setState: function(i, setState) {
+            if (setState == "open") {
+                $(accCollection[i].elementParent).removeClass('collapse');
+            } else if (setState == "close") {
+                $(accCollection[i].elementParent).addClass('collapse');
+            }
+        },
+
+        _setDefaultStates: function() {
+            for (var i = 0; i < accCollection.length; i++) {
+                if (accCollection[i].collapsed == 'true') {
+                    this._setState(i, 'close');
+                }
+            }
         },
 
         _setupAcc: function() {
-            //$(this.el);
-
-            //find all accordians
-            
-            $(accGroups).each(function () {
-
+            //find all accordions
+            $(accGroups).each(function() {
                 if ($(this).prop('nodeName') == 'H2') {
-                    $(this).on('click', function () {
+                    $(this).on('click', function() {
                         $(this).parent().next().toggle();
                     });
                 }
                 if ($(this).prop('nodeName') == 'SPAN') {
-                    $(this).on('click', function () {
+                    $(this).on('click', function() {
                         $(this).next().toggle();
                     });
                 }
-
             });
-            
-            //debugger;
         }
 
     };
+    
 
-    // Private methods start with underscore
-
-    $.fn[pluginName] = function (options) {
+    $.fn[pluginName] = function(options) {
         var args = arguments;
 
         // Is the first parameter an object (options), or was omitted,
         // instantiate a new instance of the plugin.
         if (options === undefined || typeof options === 'object') {
-            return this.each(function () {
+            return this.each(function() {
 
                 // Only allow the plugin to be instantiated once,
                 // so we check that the element has no plugin instantiation yet
@@ -135,7 +158,7 @@
             // to return a value
             var returns;
 
-            this.each(function () {
+            this.each(function() {
                 var instance = $.data(this, 'plugin_' + pluginName);
 
                 // Tests that there's already a plugin-instance
@@ -159,5 +182,11 @@
             return returns !== undefined ? returns : this;
         }
     };
-    
+
+
+
 }(jQuery, window, document));
+
+$(function() {
+    $('.wrapper').tscAccordion();
+});
